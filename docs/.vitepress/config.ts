ec0857c83c512f484f598250d8f56556fd5292bb
wxxFE <2022095028@qq.com>
2024-04-29 21:04:53 +0800

    typescript更新

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -62,6 +62,10 @@ export default defineConfig({
                 text: "基础知识",
                 items: [
                     {
+                        text: "JavaScript",
+                        link: "/JavaScript/basic/introduction"
+                    },
+                    {
                         text: "ES6标准入门",
                         link: "/ES6标准入门/ECMAScript简介及发展历史.md"
                     },
@@ -273,7 +277,388 @@ export default defineConfig({
                     ]
                 }
             ],
-            "/TypeScript/": [
+            "JavaScript": [
+                {
+                    text: "入门篇",
+                    items: [
+                        {
+                            text: "导论",
+                            link: "/JavaScript/basic/introduction"
+                        },
+                        {
+                            text: "历史",
+                            link: "/JavaScript/basic/history"
+                        },
+                        {
+                            text: "基本语法",
+                            link: "/JavaScript/basic/grammar"
+                        }
+                    ]
+                },
+                {
+                    text: "数据类型",
+                    items: [
+                        {
+                            text: "概述",
+                            link: "/JavaScript/types/general"
+                        },
+                        {
+                            text: "null，undefined 和布尔值",
+                            link: "/JavaScript/types/null-undefined-boolean"
+                        },
+                        {
+                            text: "数值",
+                            link: "/JavaScript/types/number"
+                        },
+                        {
+                            text: "字符串",
+                            link: "/JavaScript/types/string"
+                        },
+                        {
+                            text: "对象",
+                            link: "/JavaScript/types/object"
+                        },
+                        {
+                            text: "函数",
+                            link: "/JavaScript/types/function"
+                        },
+                        {
+                            text: "数组",
+                            link: "/JavaScript/types/array"
+                        }
+                    ]
+                },
+                {
+                    text: "运算符",
+                    items: [
+                        {
+                            text: "算术运算符",
+                            link: "/JavaScript/operators/arithmetic"
+                        },
+                        {
+                            text: "比较运算符",
+                            link: "/JavaScript/operators/comparison"
+                        },
+                        {
+                            text: "布尔运算符",
+                            link: "/JavaScript/operators/boolean"
+                        },
+                        {
+                            text: "二进制位运算符",
+                            link: "/JavaScript/operators/bit"
+                        },
+                        {
+                            text: "其他运算符，运算顺序",
+                            link: "/JavaScript/operators/priority"
+                        }
+                    ]
+                },
+                {
+                    text: "语法专题",
+                    items: [
+                        {
+                            text: "数据类型的转换",
+                            link: "/JavaScript/features/conversion"
+                        },
+                        {
+                            text: "错误处理机制",
+                            link: "/JavaScript/features/error"
+                        },
+                        {
+                            text: "编程风格",
+                            link: "/JavaScript/features/style"
+                        },
+                        {
+                            text: "console 对象与控制台",
+                            link: "/JavaScript/features/console"
+                        }
+                    ]
+                },
+                {
+                    text: "标准库",
+                    items: [
+                        {
+                            text: "Object 对象",
+                            link: "/JavaScript/stdlib/object"
+                        },
+                        {
+                            text: "属性描述对象",
+                            link: "/JavaScript/stdlib/attributes"
+                        },
+                        {
+                            text: "Array 对象",
+                            link: "/JavaScript/stdlib/array"
+                        },
+                        {
+                            text: "包装对象",
+                            link: "/JavaScript/stdlib/wrapper"
+                        },
+                        {
+                            text: "Boolean 对象",
+                            link: "/JavaScript/stdlib/boolean"
+                        },
+                        {
+                            text: "Number 对象",
+                            link: "/JavaScript/stdlib/number"
+                        },
+                        {
+                            text: "String 对象",
+                            link: "/JavaScript/stdlib/string"
+                        },
+                        {
+                            text: "Math 对象",
+                            link: "/JavaScript/stdlib/math"
+                        },
+                        {
+                            text: "Date 对象",
+                            link: "/JavaScript/stdlib/date"
+                        },
+                        {
+                            text: "RegExp 对象",
+                            link: "/JavaScript/stdlib/regexp"
+                        },
+                        {
+                            text: "JSON 对象",
+                            link: "/JavaScript/stdlib/json"
+                        }
+                    ]
+                },
+                {
+                    text: "面向对象编程",
+                    items: [
+                        {
+                            text: "实例对象与 new 命令",
+                            link: "/JavaScript/oop/new"
+                        },
+                        {
+                            text: "this 关键字",
+                            link: "/JavaScript/oop/this"
+                        },
+                        {
+                            text: "对象的继承",
+                            link: "/JavaScript/oop/prototype"
+                        },
+                        {
+                            text: "Object 对象的相关方法",
+                            link: "/JavaScript/oop/object"
+                        },
+                        {
+                            text: "严格模式",
+                            link: "/JavaScript/oop/strict"
+                        }
+                    ]
+                },
+                {
+                    text: "异步操作",
+                    items: [
+                        {
+                            text: "概述",
+                            link: "/JavaScript/async/general"
+                        },
+                        {
+                            text: "定时器",
+                            link: "/JavaScript/async/timer"
+                        },
+                        {
+                            text: "Promise 对象",
+                            link: "/JavaScript/async/promise"
+                        }
+                    ]
+                },
+                {
+                    text: "文档对象模型(DOM)",
+                    items: [
+                        {
+                            text: "概述",
+                            link: "/JavaScript/dom/general"
+                        },
+                        {
+                            text: " Node 接口",
+                            link: "/JavaScript/dom/node"
+                        },
+                        {
+                            text: "NodeList 接口，HTMLCollection 接口",
+                            link: "/JavaScript/dom/nodelist"
+                        },
+                        {
+                            text: "ParentNode 接口，ChildNode 接口",
+                            link: "/JavaScript/dom/parentnode"
+                        },
+                        {
+                            text: "Document 节点",
+                            link: "/JavaScript/dom/document"
+                        },
+                        {
+                            text: "Element 节点",
+                            link: "/JavaScript/dom/element"
+                        },
+                        {
+                            text: "属性的操作",
+                            link: "/JavaScript/dom/attributes"
+                        },
+                        {
+                            text: "Text 节点和 DocumentFragment 节点",
+                            link: "/JavaScript/dom/text"
+                        },
+                        {
+                            text: "CSS 操作",
+                            link: "/JavaScript/dom/css"
+                        },
+                        {
+                            text: "Mutation Observer API",
+                            link: "/JavaScript/dom/mutationobserver"
+                        }
+                    ]
+                },
+                {
+                    text: "事件",
+                    items: [
+                        {
+                            text: "EventTarget 接口",
+                            link: "/JavaScript/events/eventtarget"
+                        },
+                        {
+                            text: "事件模型",
+                            link: "/JavaScript/events/model"
+                        },
+                        {
+                            text: "Event 对象",
+                            link: "/JavaScript/events/event"
+                        },
+                        {
+                            text: "鼠标事件",
+                            link: "/JavaScript/events/mouse"
+                        },
+                        {
+                            text: "键盘事件",
+                            link: "/JavaScript/events/keyboard"
+                        },
+                        {
+                            text: "进度事件",
+                            link: "/JavaScript/events/progress"
+                        },
+                        {
+                            text: "表单事件",
+                            link: "/JavaScript/events/form"
+                        },
+                        {
+                            text: "触摸事件",
+                            link: "/JavaScript/events/touch"
+                        },
+                        {
+                            text: "拖拉事件",
+                            link: "/JavaScript/events/drag"
+                        },
+                        {
+                            text: "其他常见事件",
+                            link: "/JavaScript/events/common"
+                        },
+                        {
+                            text: "GlobalEventHandlers 接口",
+                            link: "/JavaScript/events/globaleventhandlers"
+                        }
+                    ]
+                },
+                {
+                    text: "浏览器对象模型(BOM)",
+                    items: [
+                        {
+                            text: "浏览器模型概述",
+                            link: "/JavaScript/bom/engine"
+                        },
+                        {
+                            text: "window 对象",
+                            link: "/JavaScript/bom/window"
+                        },
+                        {
+                            text: "Navigator 对象，Screen 对象",
+                            link: "/JavaScript/bom/navigator"
+                        },
+                        {
+                            text: "Cookie",
+                            link: "/JavaScript/bom/cookie"
+                        },
+                        {
+                            text: "XMLHttpRequest 对象",
+                            link: "/JavaScript/bom/xmlhttprequest"
+                        },
+                        {
+                            text: "同源限制",
+                            link: "/JavaScript/bom/same-origin"
+                        },
+                        {
+                            text: "CORS 通信",
+                            link: "/JavaScript/bom/cors"
+                        },
+                        {
+                            text: "Storage 接口",
+                            link: "/JavaScript/bom/storage"
+                        },
+                        {
+                            text: "History 对象",
+                            link: "/JavaScript/bom/history"
+                        },
+                        {
+                            text: "Location 对象，URL 对象，URLSearchParams 对象",
+                            link: "/JavaScript/bom/location"
+                        },
+                        {
+                            text: "ArrayBuffer 对象，Blob 对象",
+                            link: "/JavaScript/bom/arraybuffer"
+                        },
+                        {
+                            text: "File 对象，FileList 对象，FileReader 对象",
+                            link: "/JavaScript/bom/file"
+                        },
+                        {
+                            text: "表单，FormData 对象",
+                            link: "/JavaScript/bom/form"
+                        },
+                        {
+                            text: "IndexedDB API",
+                            link: "/JavaScript/bom/indexeddb"
+                        },
+                        {
+                            text: "Web Worker",
+                            link: "/JavaScript/bom/webworker"
+                        }
+                    ]
+                },
+                {
+                    text: "网页元素接口",
+                    items: [
+                        {
+                            text: "a",
+                            link: "/JavaScript/elements/a"
+                        },
+                        {
+                            text: "img",
+                            link: "/JavaScript/elements/image"
+                        },
+                        {
+                            text: "form",
+                            link: "/JavaScript/elements/form"
+                        },
+                        {
+                            text: "input",
+                            link: "/JavaScript/elements/input"
+                        },
+                        {
+                            text: "button",
+                            link: "/JavaScript/elements/button"
+                        },
+                        {
+                            text: "option",
+                            link: "/JavaScript/elements/option"
+                        },
+                        {
+                            text: "video，audio",
+                            link: "/JavaScript/elements/video"
+                        }
+                    ]
+                }
+            ],
+            "TypeScript": [
                 {
                     text: "TypeScript教程",
                     items: [
